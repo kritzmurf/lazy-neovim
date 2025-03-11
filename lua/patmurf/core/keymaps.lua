cd39334daf79e93dd4d6de554e196bd4a21f3517
Patrick Murphy <pmurphy@intelligentag.com>
2025-03-11 11:44:14 -0500

    added terminal support via keybinds.

--- lua/patmurf/core/keymaps.lua
+++ lua/patmurf/core/keymaps.lua
@@ -34,3 +34,8 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- terminal management
+keymap.set("n", "<leader>tt", "<cmd>split term://zsh<CR>", { desc = "Open a zsh terminal horizontally" }) -- open zsh terminal
+keymap.set("t", "<leader>ui","<cmd>term ++close<CR>", { desc = "Close the terminal session" }) --close the terminal session
+
